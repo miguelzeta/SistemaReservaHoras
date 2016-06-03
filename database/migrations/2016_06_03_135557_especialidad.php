30015100f56a18d46e5c2b4f59cd5370401d3542
miguel zuñiga <migzunig@alumnos.ubiobio.cl>
2016-06-03 10:58:16 -0300

    tabla especialidad

---
+++ database/migrations/2016_06_03_135557_especialidad.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Especialidad extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('especialidad', function (Blueprint $table) {
+            $table->string('IdEspecialidad')->unique()->notnull();
+            $table->string('NombreEspecialidad')->notnull();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
